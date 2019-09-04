daa4d19ebc7373f3ea8aa58d27f90718af42d6b8
Andre Thiago P. Rodrigues <andrethiagopr@hotmail.com>
2019-09-04 02:03:21 -0400

    Adicionar arquivos de projeto.

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -0,0 +1,45 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class ProdutoRepository : BaseRepository<Produto>,IProdutoRepository
+    {
+        public ProdutoRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public IList<Produto> GetProdutos()
+        {
+            return dbSet.ToList();
+        }
+
+        public void SaveProdutos(List<Livro> livros)
+        {
+            foreach (var item in livros)
+            {
+                if (!dbSet.Where(x=>x.Codigo == item.Codigo).Any())
+                {
+                    dbSet.Add(new Produto(item.Codigo, item.Nome, item.Preco));
+                }
+                
+            }
+            contexto.SaveChanges();
+        }
+
+
+    }
+
+    public class Livro
+    {
+        public string Codigo { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
+
+
